c6921dc4268f3df9923a7ece293277d9cf7a561b
dirsac15 <dirsac15@htlkaindorf.at>
2018-09-28 11:59:48 +0200

    added new class schueler

---
+++ Serialization_Introduction/src/Schueler.java
@@ -0,0 +1,48 @@
+
+import java.time.LocalDate;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sabrina
+ */
+public class Schueler 
+{
+    private String name;
+    private LocalDate birthday;
+
+    public Schueler(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "Schueler{" + "name=" + name + ", birthday=" + birthday + '}';
+    }
+    
+    
+    
+    
+}
